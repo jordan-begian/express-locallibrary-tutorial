bf10e97b8bc79c107af82936f9faf608d1c3551d
Jordan Begian <40347351+jordan-begian@users.noreply.github.com>
2023-11-10 15:32:44 -0500

    add query to find and return all genres
    This query will find all genres and return them sorted alphabetically

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -3,7 +3,11 @@ const asyncHandler = require("express-async-handler");
 
 // Display list of all Genre.
 exports.genreList = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre list");
+  const allGenres = await Genre.find().sort({ name: 1 }).exec();
+  res.render("genreList", {
+    title: "Genre List",
+    genreList: allGenres,
+  })
 });
 
 // Display detail page for a specific Genre.
