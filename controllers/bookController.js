4e16ab6f3dcbb581bcaee56d422e7d0caeb809e1
Jordan Begian <40347351+jordan-begian@users.noreply.github.com>
2023-11-15 19:05:12 -0500

    update bookCreatePost to create a valid book or return error

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -80,9 +80,68 @@ exports.bookCreateGet = asyncHandler(async (req, res, next) => {
 });
 
 // Handle book create on POST.
-exports.bookCreatePost = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book create POST");
-});
+exports.bookCreatePost = [
+  (req, res, next) => {
+    if (!(req.body.genre instanceof Array)) {
+      if (typeof req.body.genre === "undefined") req.body.genre = [];
+      else req.body.genre = new Array(req.body.genre);
+    }
+    next();
+  },
+
+  body("title", "Title must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("author", "Author must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("summary", "Summary must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("isbn", "ISBN must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      summary: req.body.summary,
+      isbn: req.body.isbn,
+      genre: req.body.genre
+    })
+
+    if (!errors.isEmpty()) {
+      const [allAuthors, allGenres] = await Promise.all([
+        Author.find().exec(),
+        Genre.find().exec(),
+      ]);
+
+      allGenres.map((genre) => {
+        if (book.genre.includes(genre._id)) {
+          genre.checked = "true";
+        }
+      });
+
+      res.render("bookForm", {
+        title: "Create Book",
+        authors: allAuthors,
+        genres: allGenres,
+        book: book,
+        errors: errors.array(),
+      });
+    } else {
+      await book.save();
+      res.redirect(book.url);
+    }
+  }),
+];
 
 // Display book delete form on GET.
 exports.bookDeleteGet = asyncHandler(async (req, res, next) => {
